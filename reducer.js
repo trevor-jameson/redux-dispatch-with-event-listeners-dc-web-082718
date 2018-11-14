eb0d0e7fc9394e348029afad149434cd51f30cf3
Trevor Jameson <github email address>
2018-11-14 14:20:55 -0500

    lesson learned

--- reducer.js
+++ reducer.js
@@ -1,1 +1,25 @@
 // add code snippets from README
+let state = {count: 0}
+
+function reducer(state, action){
+  switch (action.type) {
+    case 'INCREASE_COUNT':
+      return {count: state.count + 1};
+    default:
+      return state;
+  }
+}
+
+function render(){
+    let container = document.getElementById('container');
+    container.textContent = state.count;
+}
+
+function dispatch(action){
+    state = reducer(state, action);
+	render()
+}
+
+button.addEventListener('click', function(){
+    dispatch({type: 'INCREASE_COUNT'})
+})
